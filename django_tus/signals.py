c735dfa7196b48ec6fcaf9626a85605a1ec51bd1
Satendra Rai <satendrarai5@gmail.com>
2018-04-07 16:47:48 +0530

    Update signals.py
    added user_id to params

--- django_tus/signals.py
+++ django_tus/signals.py
@@ -3,6 +3,7 @@ import django.dispatch
 
 tus_upload_finished_signal = django.dispatch.Signal(
     providing_args=[
+        "user_id",
         "metadata",
         "filename",
         "upload_file_path",
